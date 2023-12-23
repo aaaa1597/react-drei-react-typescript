0ebe92e35f51146094e72258b52365b2502dfaf2
jun <jun@Ubt2200.aaaaa>
2023-12-23 17:15:57 +0900

    Dreiでまともに動かせたの初じゃね？って思うぐらい出来ているやつ

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { Suspense, useRef } from 'react';
+import { Canvas, ThreeElements, useFrame } from "@react-three/fiber";
+import { Stats, OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
 import './App.css';
 
-function App() {
+const Cube = () => {
+  const cube = useRef<THREE.Mesh>(null!)
+
+  useFrame(() => {
+    cube.current!.rotation.x += 0.01;
+    cube.current!.rotation.y += 0.01;
+  });
+
   return (
-    <div className="App">
-      hello world!!
+    <mesh ref={cube}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshNormalMaterial />
+      {/* <meshBasicMaterial color={"#992233"}/> */}
+      </mesh>
+  );
+};
+
+const Scene = () => {
+  return (
+    <>
+      <gridHelper />
+      <axesHelper />
+      <pointLight intensity={1.0} position={[5, 3, 5]} />
+      <Cube />
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <div style={{ height: "75vh", width: "75vw"}}>
+      <Canvas camera={{ near: 0.1, far: 1000, zoom: 1 }} onCreated={({ gl }) => {gl.setClearColor("#227700") }}>
+        <Stats />
+        <OrbitControls />
+        <Suspense fallback={<div>ロード中はこれが表示される</div>}>
+          <Scene />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
